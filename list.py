c07b8c4d3f3f6768f0dddec2c212995395f6b8c4
Benrobo <alumonabenaiah71@gmail.com>
2022-03-20 10:37:57 -0800

    added list

--- list.py
+++ list.py
@@ -91,4 +91,126 @@ newlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     
 # List Comprehension :- List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
 
+# Example :- Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name. Normally, you would have to do this using a for loop shown below
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+# for i in fruits:
+#     if "a" in i:
+#         newlist.append(i)
+
+# print(newlist)
+
+# With list comprehension you can do all that with only one line of code:
+
+# Syntax 
+# list = [expression for item in iterable if condition == True]
+# The expression is the current item in the iteration, but it is also the outcome, which you can manipulate before it ends up like a list item in the new list:
+
+newlist2 = [item for item in fruits if "a" not in item]
+
+# print(newlist2)
+
+# Without if
+
+newlist2 = [x for x in fruits]
+
+# print(newlist2)
+
+# using range() function to create an iterable
+
+newlist2 = [x for x in range(100) if x%3 == 0]
+
+# print(newlist2)
+
+# Set the outcome in a list
+
+newlist2 = [x.upper() for x in fruits]
+
+# print(newlist2)
+
+# You can set the outcome to whatever you like:
+
+newlist2 = ["ben" for x in fruits]
+
+# print(newlist2)
+
+newlist2 = [x if x != "banana" else "orange" for x in fruits]
+
+# print(newlist2)
+
+"""
+    The statement above is
+    
+    'Return the item if it is not banana, if it is banana return orange'
+"""
+
+
+# SORTING LIST
+
+thislist = [100, 50, 65, 82, 23]
+
+# Ascending order
+
+thislist.sort()
+# print(thislist)
+
+# Descending order :- use the keyword argument reverse = True:
+
+thislist.sort(reverse=True)
+# print(thislist)
+
+# Case Insensitive Sort
+# By default the sort() method is case sensitive, resulting in all capital letters being sorted before lower case letters:
+
+thislist = ["banana", "Orange", "kiwi", "cherry"]
+thislist.sort()
+# print(thislist)
+
+# Customize Sort Function
+# You can also customize your own function by using the keyword argument key = function.
+
+# The function will return a number that will be used to sort the list (the lowest number first):
+
+def sortfunc(n):
+    # Sort the list based on how close the number is to 50:
+    return abs(n - 50)
+
+sortedlist = [100, 50, 65, 82, 23]
+sortedlist.sort(key=sortfunc)
+
+# print(sortedlist)
+
+# Perform a case-sensitive test before sorting
+
+thislist = thislist
+thislist.sort(key=str.lower)
+# print(thislist)
+
+
+
+# Copy a List
+
+# You cant simply makie use of list2 = list1 and think you have already copied the list1 into list2. You only copied the reference copy of list1, so anything changed in list2 would reflect in list1.
+
+list1 = [1,2,3,4,5]
+
+# list2 = list1
+# list2[3] = "benrobo"
+
+# print(list2, end="\n") # [1, 2, 3, 'benrobo', 5]
+# print(list1, end="\n") # [1, 2, 3, 'benrobo', 5]
+
+# There are numbers of ways to copy a list. one of those ways is making use of the copy() method in python
+
+list3 = list1.copy()
+
+list3[3] = "benrobo"
+
+# print(list3, end="\n") # [1, 2, 3, 'benrobo', 5]
+# print(list1, end="\n") # [1, 2, 3, 4, 5]
+
+
+
 
