220fa1f6d6fc629b4f31aded6df9c33f16160816
Benrobo <alumonabenaiah71@gmail.com>
2022-03-20 14:07:26 -0800

    updated tutorial

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+
+"""
+    Dictionary
+    
+    Dictionaries are used to store data values in key:value pairs.
+
+    A dictionary is a collection which is ordered*, changeable and does not allow duplicates.
+    
+    dictionary items are ordered, mutable, and do not allow duplicate values.
+"""
--- sets.py
+++ sets.py
@@ -30,24 +30,105 @@ thisset = {"apple", "banana", "cherry"}
     Once a set is created, you cannot change its items, but you can add new items.
 """
 
+# Duplicates Not Allowed
+
+sets = {'a','b','c','a'}
+
+# print(sets) # {'a', 'b', 'c'}
+
+# Length of Sets
+
+# print(len(sets))
+
+# SET CONSTRUCTORS
+
+newsets = set(sets)
+
+# print(newsets)
+
+# Access Items
+
+# You cannot access items in a set by referring to an index or a key. But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+
+set1 = set(sets)
+
+# for x in set1:
+#     print(x)
 
 
+# ADD ITEMS TO SET
+# Once a set is created, it cant be changed. you can only make use of the add() method for adding items to a sets.
+
+set2 =  {"apple", "orange", "banana", "pineapple"}
+
+set2.add("guava")
+
+# print(set2)
+
+# Adding items from another set into a new set :- use the update() method
+
+nset = set(set2)
+tropical = {"pineapple", "mango", "papaya"}
+
+nset.update(tropical)
+
+# print(nset) # {'guava', 'mango', 'papaya', 'banana', 'orange', 'apple', 'pineapple'}
 
 
+"""
+    Add Any Iterable
+    
+    The object in the update() method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.).`
+"""
+
+nset2 = set(set2)
+names = ['ben', "benawad", "benaiah"]
+
+nset2.update(names)
+
+# print(nset2)
 
 
+"""
+    Remove Item
+    
+    To remove an item in a set, use the remove(), or the discard() method.
+"""
+
+# Remove()
+
+# If the item to remove doesnt exist, an error would be throwned. 
+
+nset3 = set(set2)
+nset3.remove("apple")
+# print(nset3)
+
+# Discard()
+
+# If the item to remove doesnt exist, no error is throwned.
+
+nset4 = set(set2)
+nset4.discard("doesnt exist")
+# print(nset4)
 
 
+# Join Set
 
+# The union() method returns a new set with all items from both sets:
 
+# set1 = {"a", "b" , "c"}
+# set2 = {1, 2, 3}
 
+# set3 = set1.union(set2)
+# print(set3)
 
+# Or making use of the update() method
 
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
 
-
-
-
-
+set1.update(set2)
+print(set1)
 
 
 
